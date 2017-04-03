0e120db913edc0436c5560511ccb339d1bb7b35d
Charlie Matina <CharlieMatina@Charlies-MacBook-Pro.local>
2017-04-04 09:51:08 +1200

    Added JS

--- js/script.js
+++ js/script.js
@@ -1,7 +1,27 @@
 var searchbutton = $('#search-button')
 var overlay = $('#overlay')
+var master = $('#master')
+var footer = $('footer')
+var input = $('input:text')
+var closebutton = $('#close-search-button')
 
+
+ jQuery(document).ready(function($) {
 
 searchbutton.click(function(){
-	overlay.fadeIn(1000);
+	overlay.fadeIn("1000");
+	overlay.css("display","block");
+});
+
+input.on("keypress", function(){
+	$(this).val('');
+	$(this).off("keypress")
+});
+
+
+closebutton.click(function(){
+	overlay.fadeOut('slow');
+
+});
+
 });
